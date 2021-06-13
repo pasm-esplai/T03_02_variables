72bcccf8661df701f6eef2ea8a7dfddca2dd3c9d
Usuario <Usuario@LAPTOP-M2PQ13GV>
2021-06-13 20:34:27 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T03_02_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 15;
+            double A = 5.55;
+            char C = 'P';
+            Console.WriteLine("Variable N = " + N);
+            Console.WriteLine("Variable A = " + A);
+            Console.WriteLine("Variable C = " + C);
+            double suma = N + A;
+            double dif = A - N;
+            int valor = (int)C;
+
+            Console.WriteLine("N+A = " + suma);
+            Console.WriteLine("A-N = " + dif);
+            Console.WriteLine("Valor numérico de " + C + " = " + valor);
+        }
+    }
+}
